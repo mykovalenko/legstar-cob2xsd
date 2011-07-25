b70747245d0201811bf9ef5c0668fc2388a6cee3
fady.moussallam <fady.moussallam@7c21e3fe-c322-11de-a759-a54c9d3330c2>
2011-07-25 09:38:13 +0000

    [maven-scm] copy for tag legstar-cob2xsd-0.3.4

---
+++ tags/legstar-cob2xsd-0.3.4/src/test/java/com/legstar/cob2xsd/Cob2XsdTaskTest.java
@@ -0,0 +1,348 @@
+/*******************************************************************************
+ * Copyright (c) 2010 LegSem.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v2.1
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *     LegSem - initial API and implementation
+ ******************************************************************************/
+package com.legstar.cob2xsd;
+
+import java.io.File;
+import java.io.PrintStream;
+import java.util.Vector;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.tools.ant.BuildEvent;
+import org.apache.tools.ant.BuildListener;
+import org.apache.tools.ant.BuildLogger;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.ProjectHelper;
+import org.apache.tools.ant.types.FileSet;
+import org.w3c.dom.Document;
+
+import com.legstar.cob2xsd.Cob2XsdModel;
+import com.legstar.cob2xsd.Cob2XsdTask;
+import com.legstar.cob2xsd.Cob2XsdModel.CodeFormat;
+import com.legstar.codegen.CodeGenUtil;
+
+/**
+ * Test the generation model.
+ * 
+ */
+public class Cob2XsdTaskTest extends AbstractXsdTester {
+
+    /** The parameters set. */
+    private Cob2XsdModel _model;
+
+    /** Logger. */
+    private final Log _log = LogFactory.getLog(getClass());
+
+    /** {@inheritDoc} */
+    public void setUp() throws Exception {
+        super.setUp();
+        try {
+            CodeGenUtil.initVelocity();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        CodeGenUtil.checkDirectory(GEN_ANT_DIR, true);
+        _model = new Cob2XsdModel();
+    }
+
+    /**
+     * Generate an ant script and run it to produce an XML Schema.
+     * 
+     * @throws Exception if ant cannot be generated
+     */
+    public void testBuildCob2Xsd() throws Exception {
+
+        _model.setProductLocation("../../../..");
+        _model.setProbeFile(new File("probe.file.tmp"));
+        _model.setCobolSourceFilePath(COBOL_SAMPLES_DIR + "/LSFILEAE");
+        _model.setCobolSourceFileEncoding("UTF-8");
+        _model.setTargetDir(GEN_XSD_DIR);
+        _model.setTargetXsdFileName("lsfileae.xsd");
+
+        _model.setTargetNamespace("http://legstar.com/test/coxb/lsfileae");
+        _model.setAddLegStarAnnotations(true);
+        /* Backward compatibility */
+        _model.setElementNamesStartWithUppercase(true);
+        _model.setQuoteIsQuote(false);
+
+        runAnt(genAntScriptAsFile());
+
+        File xsdFile = new File(GEN_XSD_DIR, _model.getTargetXsdFileName());
+        Document result = getXMLSchemaAsDoc(xsdFile);
+        Document expected = getXMLSchemaAsDoc(new File(XSD_SAMPLES_DIR, _model
+                .getTargetXsdFileName().toLowerCase()));
+        compare(xsdFile.getName(), expected, result);
+    }
+
+    /**
+     * Generate an ant script with append base file name option and run it to
+     * produce an XML Schema.
+     * 
+     * @throws Exception if ant cannot be generated
+     */
+    public void testBuildCob2XsdWithAppendBaseFileNameOption() throws Exception {
+
+        final Project project = new Project();
+        project.addBuildListener(new TestLogger());
+        project.setCoreLoader(this.getClass().getClassLoader());
+        project.init();
+        Cob2XsdTask task = new Cob2XsdTask();
+        task.setProject(project);
+        FileSet fileset = task.createFileset();
+        fileset.setProject(project);
+        fileset.setFile(new File(COBOL_SAMPLES_DIR + "/LSFILEAE"));
+        File targetXsdFile = new File(GEN_XSD_DIR, "lsfileae.xsd");
+        task.setTarget(targetXsdFile);
+        task.setTargetNamespace("http://legstar.com/test/coxb");
+        task.setAppendBaseFileNameToNamespace(true);
+        task.setAddLegStarAnnotations(true);
+        /* Backward compatibility */
+        task.setElementNamesStartWithUppercase(true);
+        task.setQuoteIsQuote(false);
+
+        task.execute();
+
+        Document result = getXMLSchemaAsDoc(targetXsdFile);
+        Document expected = getXMLSchemaAsDoc(new File(XSD_SAMPLES_DIR,
+                targetXsdFile.getName()));
+        compare(targetXsdFile.getName(), expected, result);
+    }
+
+    /**
+     * Test the COBOL formatting.
+     * 
+     * @throws Exception if test fails
+     */
+    public void testCobolFormat() throws Exception {
+        String result = genAntScriptAsString();
+        assertTrue(result.contains("codeFormat=\"FIXED_FORMAT\""));
+        assertTrue(result.contains("startColumn=\"7\""));
+        assertTrue(result.contains("endColumn=\"72\""));
+
+        _model.setStartColumn(1);
+        _model.setEndColumn(66);
+        _model.setCobolSourceFileEncoding("ISO-8859-1");
+        result = genAntScriptAsString();
+        assertTrue(result.contains("startColumn=\"1\""));
+        assertTrue(result.contains("endColumn=\"66\""));
+        assertTrue(result.contains("cobolSourceFileEncoding=\"ISO-8859-1\""));
+
+        _model.setCodeFormat(CodeFormat.FREE_FORMAT);
+        result = genAntScriptAsString();
+        assertTrue(result.contains("codeFormat=\"FREE_FORMAT\""));
+
+    }
+
+    /**
+     * Test the XSD options.
+     * 
+     * @throws Exception if test fails
+     */
+    public void testXsdOptions() throws Exception {
+        String result = genAntScriptAsString();
+        assertTrue(result.contains("xsdEncoding=\"UTF-8\""));
+        assertFalse(result.contains("targetNamespace=\""));
+        assertTrue(result.contains("mapConditionsToFacets=\"false\""));
+        assertTrue(result.contains("nameConflictPrependParentName=\"false\""));
+        assertTrue(result.contains("elementNamesStartWithUppercase=\"false\""));
+
+        _model.setXsdEncoding("ISO-8859-1");
+        _model.setTargetNamespace("test/targetNamespace");
+        _model.setMapConditionsToFacets(true);
+        _model.setNameConflictPrependParentName(true);
+        _model.setElementNamesStartWithUppercase(true);
+        result = genAntScriptAsString();
+        assertTrue(result.contains("xsdEncoding=\"ISO-8859-1\""));
+        assertTrue(result.contains("targetNamespace=\"test/targetNamespace\""));
+        assertTrue(result.contains("mapConditionsToFacets=\"true\""));
+        assertTrue(result.contains("nameConflictPrependParentName=\"true\""));
+        assertTrue(result.contains("elementNamesStartWithUppercase=\"true\""));
+
+        _model.setCustomXsltFileName("src/test/resources/xslt/alltypes.xsl");
+        result = genAntScriptAsString();
+        assertTrue(result
+                .contains("customXsltFileName=\"src/test/resources/xslt/alltypes.xsl\""));
+
+    }
+
+    /**
+     * Test the LegStar annotations options.
+     * 
+     * @throws Exception if test fails
+     */
+    public void testLegStarAnnotationsOptions() throws Exception {
+        String result = genAntScriptAsString();
+        assertTrue(result.contains("addLegStarAnnotations=\"false\""));
+
+        _model.setAddLegStarAnnotations(true);
+        result = genAntScriptAsString();
+        assertTrue(result.contains("addLegStarAnnotations=\"true\""));
+
+    }
+
+    /**
+     * Test the COBOL compiler options.
+     * 
+     * @throws Exception if test fails
+     */
+    public void testCobolCompilerOptions() throws Exception {
+        String result = genAntScriptAsString();
+        assertTrue(result.contains("currencySign=\"$\""));
+        assertTrue(result.contains("currencySymbol=\"$\""));
+        assertTrue(result.contains("nSymbolDbcs=\"false\""));
+        assertTrue(result.contains("decimalPointIsComma=\"false\""));
+        assertTrue(result.contains("quoteIsQuote=\"true\""));
+
+        _model.setCurrencySign("€");
+        _model.setCurrencySymbol("€");
+        _model.setNSymbolDbcs(true);
+        _model.setDecimalPointIsComma(true);
+        _model.setQuoteIsQuote(false);
+        result = genAntScriptAsString();
+        assertTrue(result.contains("currencySign=\"€\""));
+        assertTrue(result.contains("currencySymbol=\"€\""));
+        assertTrue(result.contains("nSymbolDbcs=\"true\""));
+        assertTrue(result.contains("decimalPointIsComma=\"true\""));
+        assertTrue(result.contains("quoteIsQuote=\"false\""));
+
+    }
+
+    /**
+     * Generates an ant script from a VLC template.
+     * 
+     * @return the script as a string
+     * @throws Exception if generation fails
+     */
+    protected String genAntScriptAsString() throws Exception {
+        File resultFile = CodeGenUtil.getFile(GEN_ANT_DIR, "build.xml");
+        _model.generateBuild(resultFile);
+
+        String result = FileUtils.readFileToString(resultFile, "UTF-8");
+
+        if (_log.isDebugEnabled()) {
+            _log.debug(result);
+        }
+        return result;
+    }
+
+    /**
+     * Generates an ant script from a VLC template.
+     * 
+     * @return the script as a string
+     * @throws Exception if generation fails
+     */
+    protected File genAntScriptAsFile() throws Exception {
+        File resultFile = CodeGenUtil.getFile(GEN_ANT_DIR, "build.xml");
+        _model.generateBuild(resultFile);
+
+        String result = FileUtils.readFileToString(resultFile, "UTF-8");
+
+        if (_log.isDebugEnabled()) {
+            _log.debug(result);
+        }
+        return resultFile;
+    }
+
+    /**
+     * Execute an ant script.
+     * 
+     * @param buildFile the ant script
+     * @throws Exception if ant script execution fails
+     */
+    protected void runAnt(final File buildFile) throws Exception {
+        final Project project = new Project();
+        project.addBuildListener(new TestLogger());
+        project.setCoreLoader(this.getClass().getClassLoader());
+        project.init();
+        ProjectHelper helper = ProjectHelper.getProjectHelper();
+        project.addReference("ant.projectHelper", helper);
+        helper.parse(project, buildFile);
+        Vector < String > targets = new Vector < String >();
+        targets.addElement(project.getDefaultTarget());
+        project.executeTargets(targets);
+    }
+
+    /**
+     * Used to route ant messages to our log.
+     * 
+     */
+    private class TestLogger implements BuildListener, BuildLogger {
+
+        /** {@inheritDoc} */
+        public void buildFinished(final BuildEvent event) {
+            if (event.getException() == null) {
+                _log.debug("Build finished.");
+            } else {
+                _log.debug("Build finished.", event.getException());
+            }
+        }
+
+        /** {@inheritDoc} */
+        public void buildStarted(final BuildEvent event) {
+            _log.debug("Build started.");
+
+        }
+
+        /** {@inheritDoc} */
+        public void messageLogged(final BuildEvent event) {
+            _log.debug(event.getMessage());
+        }
+
+        /** {@inheritDoc} */
+        public void targetFinished(final BuildEvent event) {
+            _log.debug("targetFinished.");
+            if (event.getException() == null) {
+                _log.debug("targetFinished " + event.getTarget().getName());
+            } else {
+                _log.debug("targetFinished " + event.getTarget().getName(),
+                        event.getException());
+            }
+        }
+
+        /** {@inheritDoc} */
+        public void targetStarted(final BuildEvent event) {
+            _log.debug("targetStarted " + event.getTarget().getName());
+        }
+
+        /** {@inheritDoc} */
+        public void taskFinished(final BuildEvent event) {
+            _log.debug("taskFinished.");
+        }
+
+        /** {@inheritDoc} */
+        public void taskStarted(final BuildEvent event) {
+            _log.debug("taskStarted.");
+        }
+
+        /** {@inheritDoc} */
+        public void setEmacsMode(final boolean emacsMode) {
+            _log.debug("setEmacsMode.");
+        }
+
+        /** {@inheritDoc} */
+        public void setErrorPrintStream(final PrintStream err) {
+            _log.debug("setErrorPrintStream.");
+        }
+
+        /** {@inheritDoc} */
+        public void setMessageOutputLevel(final int level) {
+            _log.debug("setMessageOutputLevel.");
+        }
+
+        /** {@inheritDoc} */
+        public void setOutputPrintStream(final PrintStream output) {
+            _log.debug("setMessageOutputLevel.");
+        }
+
+    }
+
+}
---
+++ tags/legstar-cob2xsd-0.3.4/src/test/java/com/legstar/cob2xsd/XsdAnnotationEmitterTest.java
@@ -0,0 +1,368 @@
+/*******************************************************************************
+ * Copyright (c) 2010 LegSem.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v2.1
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *     LegSem - initial API and implementation
+ ******************************************************************************/
+package com.legstar.cob2xsd;
+
+import java.util.ArrayList;
+
+import org.apache.ws.commons.schema.XmlSchema;
+import org.apache.ws.commons.schema.XmlSchemaAnnotation;
+
+import com.legstar.cobol.RecognizerErrorHandler;
+import com.legstar.cobol.model.CobolDataItem;
+import com.legstar.cobol.model.CobolDataItem.Usage;
+
+/**
+ * Test the XSD annotations produced.
+ * 
+ */
+public class XsdAnnotationEmitterTest extends AbstractXsdEmitterTester {
+
+    /** Handles error messages. */
+    private RecognizerErrorHandler _errorHandler = new RecognizerErrorHandler();
+
+    /**
+     * Test a group item.
+     */
+    public void testGroupItem() {
+        CobolDataItem dataItem = new CobolDataItem("COBOL-NAME");
+        dataItem.getChildren().add(new CobolDataItem("CHILD-NAME"));
+        emitAnnotationAndCheck(
+                dataItem,
+                "<cb:cobolElement cobolName=\"COBOL-NAME\" levelNumber=\"1\" type=\"GROUP_ITEM\"/>");
+    }
+
+    /**
+     * Test an elementary item with usage.
+     */
+    public void testUsage() {
+        CobolDataItem dataItem = new CobolDataItem("COBOL-NAME");
+        dataItem.setUsage(Usage.DOUBLEFLOAT);
+        emitAnnotationAndCheck(dataItem,
+                "<cb:cobolElement cobolName=\"COBOL-NAME\""
+                        + " levelNumber=\"1\"" + " type=\"DOUBLE_FLOAT_ITEM\""
+                        + " usage=\"COMP-2\"/>");
+    }
+
+    /**
+     * Test an elementary item with picture.
+     */
+    public void testPicture() {
+        CobolDataItem dataItem = new CobolDataItem("COBOL-NAME");
+        dataItem.setPicture("NNNN/NN");
+        dataItem.setUsage(Usage.NATIONAL);
+        emitAnnotationAndCheck(dataItem,
+                "<cb:cobolElement cobolName=\"COBOL-NAME\""
+                        + " levelNumber=\"1\"" + " picture=\"NNNN/NN\""
+                        + " type=\"NATIONAL_ITEM\"" + " usage=\"NATIONAL\"/>");
+    }
+
+    /**
+     * Test an elementary item with justified right.
+     */
+    public void testJustifiedRight() {
+        CobolDataItem dataItem = new CobolDataItem("COBOL-NAME");
+        dataItem.setPicture("X");
+        dataItem.setJustifiedRight(true);
+        emitAnnotationAndCheck(dataItem,
+                "<cb:cobolElement cobolName=\"COBOL-NAME\""
+                        + " levelNumber=\"1\"" + " picture=\"X\""
+                        + " type=\"ALPHANUMERIC_ITEM\""
+                        + " justifiedRight=\"true\"/>");
+    }
+
+    /**
+     * Test an elementary item with total digits.
+     */
+    public void testTotalDigits() {
+        CobolDataItem dataItem = new CobolDataItem("COBOL-NAME");
+        dataItem.setPicture("99");
+        dataItem.setUsage(Usage.PACKEDDECIMAL);
+        emitAnnotationAndCheck(dataItem,
+                "<cb:cobolElement cobolName=\"COBOL-NAME\""
+                        + " levelNumber=\"1\"" + " picture=\"99\""
+                        + " type=\"PACKED_DECIMAL_ITEM\""
+                        + " usage=\"PACKED-DECIMAL\"" + " totalDigits=\"2\""
+                        + " signed=\"false\"" + "/>");
+    }
+
+    /**
+     * Test an elementary item with fraction digits.
+     */
+    public void testFractionDigits() {
+        CobolDataItem dataItem = new CobolDataItem("COBOL-NAME");
+        dataItem.setPicture("S99V9");
+        emitAnnotationAndCheck(dataItem,
+                "<cb:cobolElement cobolName=\"COBOL-NAME\""
+                        + " levelNumber=\"1\"" + " picture=\"S99V9\""
+                        + " type=\"ZONED_DECIMAL_ITEM\"" + " totalDigits=\"3\""
+                        + " fractionDigits=\"1\"" + " signed=\"true\"" + "/>");
+    }
+
+    /**
+     * Test an elementary item with sign leading.
+     */
+    public void testSignLeading() {
+        CobolDataItem dataItem = new CobolDataItem("COBOL-NAME");
+        dataItem.setPicture("S99V9");
+        dataItem.setSignLeading(true);
+        emitAnnotationAndCheck(dataItem,
+                "<cb:cobolElement cobolName=\"COBOL-NAME\""
+                        + " levelNumber=\"1\"" + " picture=\"S99V9\""
+                        + " type=\"ZONED_DECIMAL_ITEM\"" + " totalDigits=\"3\""
+                        + " fractionDigits=\"1\"" + " signed=\"true\""
+                        + " signLeading=\"true\"" + "/>");
+    }
+
+    /**
+     * Test an elementary item with sign separate.
+     */
+    public void testSignSeparate() {
+        CobolDataItem dataItem = new CobolDataItem("COBOL-NAME");
+        dataItem.setPicture("S99V9");
+        dataItem.setSignLeading(true);
+        dataItem.setSignSeparate(true);
+        emitAnnotationAndCheck(dataItem,
+                "<cb:cobolElement cobolName=\"COBOL-NAME\""
+                        + " levelNumber=\"1\"" + " picture=\"S99V9\""
+                        + " type=\"ZONED_DECIMAL_ITEM\"" + " totalDigits=\"3\""
+                        + " fractionDigits=\"1\"" + " signed=\"true\""
+                        + " signLeading=\"true\"" + " signSeparate=\"true\""
+                        + "/>");
+    }
+
+    /**
+     * Test an array item.
+     */
+    public void testArray() {
+        CobolDataItem dataItem = new CobolDataItem("COBOL-NAME");
+        dataItem.setMaxOccurs(3);
+        emitAnnotationAndCheck(dataItem,
+                "<cb:cobolElement cobolName=\"COBOL-NAME\""
+                        + " levelNumber=\"1\"" + " maxOccurs=\"3\""
+                        + " minOccurs=\"3\"" + " type=\"GROUP_ITEM\"" + "/>");
+    }
+
+    /**
+     * Test an array item with depending on.
+     */
+    public void testArrayDependingOn() {
+        CobolDataItem dataItem = new CobolDataItem("COBOL-NAME");
+        dataItem.setMinOccurs(0);
+        dataItem.setMaxOccurs(1);
+        dataItem.setDependingOn("COBOL-LEN");
+        emitAnnotationAndCheck(dataItem,
+                "<cb:cobolElement cobolName=\"COBOL-NAME\""
+                        + " levelNumber=\"1\"" + " dependingOn=\"COBOL-LEN\""
+                        + " maxOccurs=\"1\"" + " minOccurs=\"0\""
+                        + " type=\"GROUP_ITEM\"" + "/>");
+    }
+
+    /**
+     * Test an item redefining another.
+     */
+    public void testRedefines() {
+        CobolDataItem dataItem = new CobolDataItem("COBOL-REDEFINING");
+        dataItem.setRedefines("COBOL-REDEFINED");
+        emitAnnotationAndCheck(dataItem,
+                "<cb:cobolElement cobolName=\"COBOL-REDEFINING\""
+                        + " levelNumber=\"1\""
+                        + " redefines=\"COBOL-REDEFINED\""
+                        + " type=\"GROUP_ITEM\"" + "/>");
+    }
+
+    /**
+     * Test an item with value ZERO.
+     */
+    public void testValueZERO() {
+        CobolDataItem dataItem = new CobolDataItem("COBOL-NAME");
+        dataItem.setValue("ZEROS");
+        emitAnnotationAndCheck(dataItem,
+                "<cb:cobolElement cobolName=\"COBOL-NAME\""
+                        + " levelNumber=\"1\"" + " value=\"0\""
+                        + " type=\"GROUP_ITEM\"" + "/>");
+    }
+
+    /**
+     * Test an item with value SPACE.
+     */
+    public void testValueSPACE() {
+        CobolDataItem dataItem = new CobolDataItem("COBOL-NAME");
+        dataItem.setValue("SPACE");
+        emitAnnotationAndCheck(dataItem,
+                "<cb:cobolElement cobolName=\"COBOL-NAME\""
+                        + " levelNumber=\"1\"" + " value=\" \""
+                        + " type=\"GROUP_ITEM\"" + "/>");
+    }
+
+    /**
+     * Test an item with value HIGH-VALUE.
+     */
+    public void testValueHIGHVALUE() {
+        CobolDataItem dataItem = new CobolDataItem("COBOL-NAME");
+        dataItem.setPicture("X(3)");
+        dataItem.setValue("HIGH-VALUES");
+        emitAnnotationAndCheck(dataItem,
+                "<cb:cobolElement cobolName=\"COBOL-NAME\""
+                        + " levelNumber=\"1\"" + " picture=\"X(3)\""
+                        + " value=\"0xFFFFFF\"" + " type=\"ALPHANUMERIC_ITEM\""
+                        + "/>");
+    }
+
+    /**
+     * Test an item with value LOW-VALUE.
+     */
+    public void testValueLOWVALUE() {
+        CobolDataItem dataItem = new CobolDataItem("COBOL-NAME");
+        dataItem.setPicture("X(3)");
+        dataItem.setValue("LOW-VALUES");
+        emitAnnotationAndCheck(dataItem,
+                "<cb:cobolElement cobolName=\"COBOL-NAME\""
+                        + " levelNumber=\"1\"" + " picture=\"X(3)\""
+                        + " value=\"0x000000\"" + " type=\"ALPHANUMERIC_ITEM\""
+                        + "/>");
+    }
+
+    /**
+     * Test an item with value LOW-VALUE.
+     */
+    public void testValueNULLS() {
+        CobolDataItem dataItem = new CobolDataItem("COBOL-NAME");
+        dataItem.setPicture("X(3)");
+        dataItem.setValue("NULLS");
+        emitAnnotationAndCheck(dataItem,
+                "<cb:cobolElement cobolName=\"COBOL-NAME\""
+                        + " levelNumber=\"1\"" + " picture=\"X(3)\""
+                        + " value=\"0x000000\"" + " type=\"ALPHANUMERIC_ITEM\""
+                        + "/>");
+    }
+
+    /**
+     * Test an item with value QUOTES.
+     */
+    public void testValueQUOTES() {
+        CobolDataItem dataItem = new CobolDataItem("COBOL-NAME");
+        dataItem.setPicture("X(3)");
+        dataItem.setValue("QUOTES");
+        emitAnnotationAndCheck(dataItem,
+                "<cb:cobolElement cobolName=\"COBOL-NAME\""
+                        + " levelNumber=\"1\"" + " picture=\"X(3)\""
+                        + " value=\"&quot;\"" + " type=\"ALPHANUMERIC_ITEM\""
+                        + "/>");
+    }
+
+    /**
+     * Test an item with value APOST.
+     */
+    public void testValueAPOST() {
+        CobolDataItem dataItem = new CobolDataItem("COBOL-NAME");
+        dataItem.setPicture("X(3)");
+        dataItem.setValue("apost");
+        emitAnnotationAndCheck(dataItem,
+                "<cb:cobolElement cobolName=\"COBOL-NAME\""
+                        + " levelNumber=\"1\"" + " picture=\"X(3)\""
+                        + " value=\"&apos;\"" + " type=\"ALPHANUMERIC_ITEM\""
+                        + "/>");
+    }
+
+    /**
+     * Test an item with value ALL.
+     */
+    public void testValueALL() {
+        CobolDataItem dataItem = new CobolDataItem("COBOL-NAME");
+        dataItem.setPicture("X(3)");
+        dataItem.setValue("ALL A");
+        emitAnnotationAndCheck(dataItem,
+                "<cb:cobolElement cobolName=\"COBOL-NAME\""
+                        + " levelNumber=\"1\"" + " picture=\"X(3)\""
+                        + " value=\"AAA\"" + " type=\"ALPHANUMERIC_ITEM\""
+                        + "/>");
+    }
+
+    /**
+     * Test an item with value ALL applied to a figurative constant.
+     */
+    public void testValueALLFigurative() {
+        CobolDataItem dataItem = new CobolDataItem("COBOL-NAME");
+        dataItem.setPicture("X(3)");
+        dataItem.setValue("ALL QUOTE");
+        emitAnnotationAndCheck(dataItem,
+                "<cb:cobolElement cobolName=\"COBOL-NAME\""
+                        + " levelNumber=\"1\"" + " picture=\"X(3)\""
+                        + " value=\"&quot;&quot;&quot;\""
+                        + " type=\"ALPHANUMERIC_ITEM\"" + "/>");
+    }
+
+    /**
+     * Test an item with a numeric.
+     */
+    public void testValueNumeric() {
+        CobolDataItem dataItem = new CobolDataItem("COBOL-NAME");
+        dataItem.setUsage(Usage.PACKEDDECIMAL);
+        dataItem.setValue("-125.63");
+        emitAnnotationAndCheck(dataItem,
+                "<cb:cobolElement cobolName=\"COBOL-NAME\""
+                        + " levelNumber=\"1\"" + " usage=\"PACKED-DECIMAL\""
+                        + " value=\"-125.63\""
+                        + " type=\"PACKED_DECIMAL_ITEM\"" + "/>");
+    }
+
+    /**
+     * Test an item with a alpha escaped.
+     */
+    public void testValueEscaped() {
+        CobolDataItem dataItem = new CobolDataItem("COBOL-NAME");
+        dataItem.setPicture("X(3)");
+        dataItem.setValue("<A>");
+        emitAnnotationAndCheck(dataItem,
+                "<cb:cobolElement cobolName=\"COBOL-NAME\""
+                        + " levelNumber=\"1\"" + " picture=\"X(3)\""
+                        + " value=\"&lt;A&gt;\""
+                        + " type=\"ALPHANUMERIC_ITEM\"" + "/>");
+    }
+
+    /**
+     * Test an item with a value that is a delimited literal.
+     */
+    public void testValueDelimitedLiteral() {
+        CobolDataItem dataItem = new CobolDataItem("COBOL-NAME");
+        dataItem.setPicture("X(3)");
+        dataItem.setValue("\"ABD\"");
+        emitAnnotationAndCheck(dataItem,
+                "<cb:cobolElement cobolName=\"COBOL-NAME\""
+                        + " levelNumber=\"1\"" + " picture=\"X(3)\""
+                        + " value=\"ABD\"" + " type=\"ALPHANUMERIC_ITEM\""
+                        + "/>");
+    }
+
+    /**
+     * Helper to check item level annotation emission. Get item level
+     * annotations. We hook them directly in the schema because that simplifies
+     * testing.
+     * 
+     * @param dataItem a COBOL data item
+     * @param expected expected annotations as a string
+     */
+    private void emitAnnotationAndCheck(final CobolDataItem dataItem,
+            final String expected) {
+        Cob2XsdModel model = new Cob2XsdModel();
+        model.setAddLegStarAnnotations(true);
+        XmlSchema xsd = getXmlSchema();
+        XsdAnnotationEmitter emitter = new XsdAnnotationEmitter(xsd, model);
+        XsdDataItem xsdDataItem = new XsdDataItem(dataItem, model, null, 0,
+                new ArrayList < String >(), _errorHandler);
+        XmlSchemaAnnotation annotation = emitter
+                .createLegStarAnnotation(xsdDataItem);
+        xsd.setAnnotation(annotation);
+        check("<annotation>" + "<appinfo>" + expected + "</appinfo>"
+                + "</annotation>", xsd, true);
+    }
+
+}
